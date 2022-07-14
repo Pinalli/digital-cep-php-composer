b53ff240f785e57adae7ce6b15a84da393ebe66b
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-07-14 11:02:56 -0300

    install composer

--- src/Search.php
+++ src/Search.php
@@ -1,7 +1,18 @@
 <?php
 
-namespace DigitalCep\Search;
+namespace Search\DigitalCep;
 
 class Search{
+    
+    private $url "https://viacep.com.br/ws/";
+
+    public function getAddressFromZipCode(string $zipcode): array{
+        //usada para realizar uma expressão regular para pesquisar e substituir o conteúdo
+        $zipCode = preg_replace('/[^0-9]/in', '', $zipcode);
+        $get =file_get_contents($this->url . $zipCode . "/json/");
+
+        print_r($get);// produz as informações detalhadas sobre o parâmetro em um formato com seu tipo (de uma matriz ou um objeto)
+       
+    }
 
 }
