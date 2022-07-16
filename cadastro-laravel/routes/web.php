72ecdd28d5aa08748750eda7951468700c20b85c
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-07-15 21:44:19 -0300

    controllers

--- cadastro-laravel/routes/web.php
+++ cadastro-laravel/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +12,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\Usuario@cadastrar');
+Route::post('/salvar', 'App\Http\Controllers\Usuario@salvar');
